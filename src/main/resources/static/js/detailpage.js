3c5250b80a0e105622087d3cdd92079ab710bdd9
parkseb <parkseb@daum.net>
2023-06-12 18:32:54 +0900

    alalalal

---
+++ src/main/resources/static/js/detailpage.js
@@ -0,0 +1,15 @@
+<!-- 슬라이드 -->
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    slideIndex += n;
+    if (slideIndex > 4) {
+        slideIndex = 1;
+    } else if (slideIndex < 1) { slideIndex = 4; } showSlides(slideIndex);
+} function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("Reviewgreen"); for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    } slides[slideIndex - 1].style.display = "block";
+}
