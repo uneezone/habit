82bfb3b29ecb1a387dd1a11d19056d382f36770d
ohruha <85723624+ohruha@users.noreply.github.com>
2023-06-15 17:18:58 +0900

    fixed untracked files

--- src/main/java/com/habit/zzim/ZzimCont.java
+++ src/main/java/com/habit/zzim/ZzimCont.java
@@ -3,27 +3,74 @@ package com.habit.zzim;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/zzim")
 public class ZzimCont {
 
     public ZzimCont() {
-        System.out.println("----CartCont() 객체 생성됨");
+        System.out.println("----ZzimCont() 객체 생성됨");
     }
 
     @Autowired
-    ZzimDAO zzimDAO;
+    ZzimDAO zzimDao;
 
-    @RequestMapping("/list")
+    @RequestMapping("/list") //@RequestParam MultipartFile cont_img
     public ModelAndView list(HttpSession session, HttpServletRequest req){
-        String id="test";
+        //@ModelAttribute ZzimDTO dto(요거 필요없음. 없어도 값 받아져있음)
+        // @RequestParam Map<String, Object> map(요것도 필요없음. 값 잘 받아져있음)
 
+        String user_id;
+        user_id="user-2";
+        //test
+        List<Map<String, Object>> zzimlist = zzimDao.zzimlist(user_id);
+
+        for (Map<String, Object> stringObjectMap : zzimlist) {
+            System.out.println(stringObjectMap);
+        }
+        //test용 출력문
+        /*
+        for (Map<String, Object> stringObjectMap : zzimlist) {
+            System.out.println("stringObjectMap = " + stringObjectMap);
+            System.out.println("id"+stringObjectMap.get("user_id"));
+            if(user_id.equals(stringObjectMap.get("user_id"))){
+                real.add(stringObjectMap);
+            }
+        }
+        */
+        //System.out.println("real="+real.stream().toList());
+        //zzimlist에 찜리스트 정보 몽땅 들어있음.
+        /*
+        HashMap<String, Object> map2=new HashMap<>();
+        map2.put("user_id", user_id);
+        map2.put("zzimlist", zzimDao.zzimlist());
+*/
+        /*for (String s : map2.keySet()) {
+            System.out.println(s);
+        }*/
+
+        //System.out.println(map2.get("user_id"));
+        //System.out.println(map2.get("zzimlist"));
+
+
+
+        ModelAndView mav=new ModelAndView();
+        mav.setViewName("member/zzim");
+        mav.addObject("zzim", zzimlist);
+
+        return mav;
     }
 
 
