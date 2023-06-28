a7d7510d5ecc204dbd94cc89bc09b97daebf23d5
HaeBeen <haebing0309@gmail.com>
2023-06-29 08:16:35 +0900

    로그인 인터셉터

---
+++ src/main/java/com/habit/WebConfig.java
@@ -0,0 +1,21 @@
+package com.habit;
+
+import com.habit.Interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+/*   @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/","/category/**","/category","/login","/logout.do","/findMember","/resultFindId","/resultFind"
+                        ,"/findid.do","/css/*","/js/*","/img/*","/products/reviews","/search/**","/storage/*","/newMember","/idCheck","/welcome.do"
+                );
+    }*/
+}
